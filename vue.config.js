6939e46b87eadbaabf1f01dd89a38a64d7c8a3eb
jeffrey <piccirilloj1@gmail.com>
2022-12-30 15:17:51 -0600

    Add manual preview / embedded meta tags for SNORE questionnaire

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
+const webpackParams = {
+  title:
+    'Symptoms of Nocturnal Obstruction and Related Events (SNORE) Questionnare',
+  description:
+    "Evaluate the extent of one's physical problems, limitations, and emotional consequences of sleep disorder-related health burdens with a 25-question inventory.",
+  site_name:
+    'Applets from Clinical Outcomes Research at Washington University in St Louis',
+  url: 'https://outcomesresearch.github.io/snore/',
+};
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/snore/' : '/',
   transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      Object.entries(webpackParams).forEach(([key, value]) => {
+        args[0][key] = value;
+      });
+      return args;
+    });
+  },
 };
