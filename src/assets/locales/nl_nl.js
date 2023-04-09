35326b629793d5b586bc1fd41c3992247c34540d
jeffrey <piccirilloj1@gmail.com>
2023-04-09 13:14:47 -0500

    Add Dutch translation strings

--- src/assets/locales/nl_nl.js
+++ src/assets/locales/nl_nl.js
@@ -1,8 +1,127 @@
 import keys from './keys.js';
 import { nl } from 'vuetify/lib/locale/';
 
-keys;
-
 export default {
   ...nl, // existing vuetify translation entries
+
+  [keys.FOOTER_COPYRIGHT]:
+    'Copyright © 1996 by Jay F. Piccirillo, M. D., Washington University School of Medicine, St. Louis, Missouri',
+  [keys.INTERPRETATION_MILD]: 'Mild',
+  [keys.INTERPRETATION_MODERATE]: 'Gematigd',
+  [keys.INTERPRETATION_SEVERE]: 'Ernstig',
+
+  [keys.HEADER_TITLE]:
+    'Symptomen Van Nachtelijke Obstructie en Geassocieerde Voorvallen (SNURKEN)',
+  [keys.HEADER_BODY_1]:
+    'Hieronder staat een lijst van symptomen en de sociale/emotionele gevolgen met betrekking tot uw ziekte. We willen graag meer weten over deze problemen en stellen het op prijs als u de onderstaande vragen zo goed mogelijk beantwoordt. Er zijn geen goede of foute antwoorden en alleen u kunt ons deze informatie verschaffen. Beoordeel de problemen waarvan u in de afgelopen ',
+  [keys.HEADER_BODY_2]: 'twee weken last',
+  [keys.HEADER_BODY_3]: ' had.',
+  [keys.HEADER_BODY_4]: 'Vriendelijk bedankt voor uw medewerking.',
+  [keys.SECTION1_INSTRUCTIONS_1]:
+    'Denk aan de ernst en frequentie van uw probleem wanneer u er last van hebt. Beoordeel vervolgens elke bewering hieronder op basis van de ernst van het probleem en omcirkel het cijfer dat overeenkomt met wat u voelt:',
+  [keys.TOP_5_INSTRUCTIONS]:
+    'Vink de items aan die de meeste invloed hebben op uw gezondheid (maximum 5 items).',
+  [keys.SECTION1_OPTION_1]: 'Geen probleem',
+  [keys.SECTION1_OPTION_2]: 'Zeer klein probleem',
+  [keys.SECTION1_OPTION_3]: 'Klein of minimaal probleem',
+  [keys.SECTION1_OPTION_4]: 'Gemiddeld probleem',
+  [keys.SECTION1_OPTION_5]: 'Ernstig probleem',
+  [keys.SECTION1_OPTION_6]: 'Ergst mogelijke probleem',
+
+  [keys.SECTION1_PROMPT_1]:
+    'Wakker worden in de nacht; niet in staat om een goede nachtrust te hebben',
+  [keys.SECTION1_PROMPT_2]: 'Luid/overmatig snurken',
+  [keys.SECTION1_PROMPT_3]: 'Rusteloos tijdens het slapen',
+  [keys.SECTION1_PROMPT_4]: 'Vermoeid wakker worden',
+  [keys.SECTION1_PROMPT_5]:
+    'Uitgeput, vermoeid (bijv: in slaap vallen tijdens het lezen van een boek)',
+  [keys.SECTION1_PROMPT_6]: 'Vaak gapen',
+  [keys.SECTION1_PROMPT_7]: 'Vermoeidheid achter het stuur',
+  [keys.SECTION1_PROMPT_8]: 'Problemen met geheugen en/of concentratie',
+  [keys.SECTION1_PROMPT_9]:
+    'Beperkt de productiviteit op bepaalde momenten van de dag (bijv: in de avond na het werk)',
+  [keys.SECTION1_PROMPT_10]:
+    'Beperkt uw deelname aan de gemeenschap, vrijwilligerswerk, religieuze of spirituele activiteiten',
+  [keys.SECTION1_PROMPT_11]:
+    'De hoeveelheid medische zorg rondom de slaapstoornissen (bijv: veelvuldige doktersbezoeken, het dragen van een masker met positieve druk, het schoonmaken van de luchtpijpbuis, enz.)',
+  [keys.SECTION1_PROMPT_12]:
+    'Interactie tussen obstructieve slaapstoornissen en andere medische problemen',
+  [keys.SECTION1_PROMPT_13]:
+    "Angst voor medische problemen beperkt autoritten naar andere regio's of delen van het land.",
+  [keys.SECTION1_PROMPT_14]: 'Belemmering van seksueel contact',
+  [keys.SECTION1_PROMPT_15]:
+    'Financiële last door de ziekte (bijv: kosten van medicijnen, doktersbezoeken, laboratoriumonderzoeken, enz.)',
+  [keys.SECTION1_PROMPT_16]:
+    'Erg snel geprikkeld; kort lontje, schiet snel uit zijn of haar slof',
+  [keys.SECTION1_PROMPT_17]: 'Niet in staat om te ontspannen; altijd bezorgd',
+  [keys.SECTION1_PROMPT_18]:
+    'Spanningen binnen het huwelijk, stress en spanningen',
+  [keys.SECTION1_PROMPT_19]: 'Slecht gehumeurd',
+  [keys.SECTION1_PROMPT_20]:
+    'Niet in staat om een hechte relatie te onderhouden met partner en/of andere dierbaren',
+  [keys.SECTION1_PROMPT_21]: 'Verminderd verlangen naar seksueel contact',
+  [keys.SECTION1_PROMPT_22]: 'Gevoel dat er geen hoop voor de toekomst is',
+  [keys.SECTION1_PROMPT_23]: 'Vaardigheden worden in twijfel getrokken',
+  [keys.SECTION1_PROMPT_24]: 'Geloofwaardigheid wordt in twijfel getrokken',
+  [keys.SECTION1_PROMPT_25]:
+    'Veranderingen in uw werk als gevolg van de ziekte (bijv: iemand bestuurt de auto in plaats van u, onvermogen om deel te nemen aan vergaderingen als gevolg van overmatige slaperigheid)',
+
+  [keys.AVERAGE]: 'Gemiddeld',
+  [keys.SUM]: 'Som',
+  [keys.SCORE]: 'Scoren',
+  [keys.SCORE_WHEN_FORM_COMPLETE]:
+    'Het resultaat wordt hier weergegeven wanneer het formulier is voltooid.',
+
+  [keys.INTERPRETATION_1]:
+    'Wanneer SNORE-scores meerdere keren worden ingenomen, kunnen ze worden gebruikt om de verandering in fysieke en functionele problemen, functionele beperkingen en emotionele gevolgen van aan slaapstoornissen gerelateerde gezondheidslasten in de loop van de tijd en als gevolg van de behandeling te meten.',
+  [keys.INTERPRETATION_2]:
+    'De impact van de behandeling wordt beoordeeld met de SNORE Change Score, die het verschil is tussen de SNORE pre-treatment en SNORE post-Treatment gemiddelde scores.  Een SNORE-25-veranderingsscore van 0,5 kan worden gezien als een minimaal belangrijk klinisch verschil en veranderingsscores hoger dan deze waarde moet als klinisch van belang worden bekeken.',
+
+  [keys.INTERPRETATION_TITLE]: 'SNORE Score Interpretatie',
+
+  [keys.BASED_ON_AVERAGE]: 'Op basis van gemiddelde',
+  [keys.BASED_ON_SUM]: 'Op basis van som',
+
+  // Shortened versions of prompts to use in grid
+  [keys.SECTION1_PROMPT_1_SHORT]:
+    'Wakker worden in de nacht; niet in staat om een goede nachtrust te hebben',
+  [keys.SECTION1_PROMPT_2_SHORT]: 'Luid/overmatig snurken',
+  [keys.SECTION1_PROMPT_3_SHORT]: 'Rusteloos tijdens het slapen',
+  [keys.SECTION1_PROMPT_4_SHORT]: 'Vermoeid wakker worden',
+  [keys.SECTION1_PROMPT_5_SHORT]: 'Uitgeput, vermoeid',
+  [keys.SECTION1_PROMPT_6_SHORT]: 'Vaak gapen',
+  [keys.SECTION1_PROMPT_7_SHORT]: 'Vermoeidheid achter het stuur',
+  [keys.SECTION1_PROMPT_8_SHORT]: 'Problemen met geheugen en/of concentratie',
+  [keys.SECTION1_PROMPT_9_SHORT]:
+    'Beperkt de productiviteit op bepaalde momenten van de dag',
+  [keys.SECTION1_PROMPT_10_SHORT]:
+    'Beperkt uw deelname aan de gemeenschap, vrijwilligerswerk, religieuze of spirituele activiteiten',
+  [keys.SECTION1_PROMPT_11_SHORT]:
+    'De hoeveelheid medische zorg rondom de slaapstoornissen',
+  [keys.SECTION1_PROMPT_12_SHORT]:
+    'Interactie tussen obstructieve slaapstoornissen en andere medische problemen',
+  [keys.SECTION1_PROMPT_13_SHORT]:
+    "Angst voor medische problemen beperkt autoritten naar andere regio's of delen van het land.",
+  [keys.SECTION1_PROMPT_14_SHORT]: 'Belemmering van seksueel contact',
+  [keys.SECTION1_PROMPT_15_SHORT]: 'Financiële last door de ziekte',
+  [keys.SECTION1_PROMPT_16_SHORT]:
+    'Erg snel geprikkeld; kort lontje, schiet snel uit zijn of haar slof',
+  [keys.SECTION1_PROMPT_17_SHORT]:
+    'Niet in staat om te ontspannen; altijd bezorgd',
+  [keys.SECTION1_PROMPT_18_SHORT]:
+    'Spanningen binnen het huwelijk, stress en spanningen',
+  [keys.SECTION1_PROMPT_19_SHORT]: 'Slecht gehumeurd',
+  [keys.SECTION1_PROMPT_20_SHORT]:
+    'Niet in staat om een hechte relatie te onderhouden met partner en/of andere dierbaren',
+  [keys.SECTION1_PROMPT_21_SHORT]: 'Verminderd verlangen naar seksueel contact',
+  [keys.SECTION1_PROMPT_22_SHORT]:
+    'Gevoel dat er geen hoop voor de toekomst is',
+  [keys.SECTION1_PROMPT_23_SHORT]: 'Vaardigheden worden in twijfel getrokken',
+  [keys.SECTION1_PROMPT_24_SHORT]:
+    'Geloofwaardigheid wordt in twijfel getrokken',
+  [keys.SECTION1_PROMPT_25_SHORT]:
+    'Veranderingen in uw werk als gevolg van de ziekte',
+
+  [keys.ONLY_INCLUDE_TOP_5]:
+    'Voeg alleen items toe die het meest van invloed zijn op uw gezondheid',
 };
